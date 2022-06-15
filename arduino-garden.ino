74abd10cab8a40895a988a92cb3c3511320bcfe6
Jordan Penard <jordan038@gmail.com>
2022-06-15 23:22:13 +0100

    Added a web server that display the logged data as a graph

--- arduino-garden.ino
+++ arduino-garden.ino
@@ -3,13 +3,17 @@
 //
 
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
 #include "config.h"
 
+
+// -----------
 // Timer stuff
-//
+
 #include "ESP8266TimerInterrupt.h"
 #include "ESP8266_ISR_Timer.h"
 
@@ -26,11 +30,178 @@ ESP8266Timer ITimer;
 ESP8266_ISR_Timer ISR_Timer;
 
 
+// -----------
+// HTTP server
+
+ESP8266WebServer server(80);
+
+static const char TEXT_PLAIN[] PROGMEM = "text/plain";
+
+
+// -----------
+// Data loging
+
+int history_moisture[HISTORY_DEPTH];
+bool history_pump[HISTORY_DEPTH];
+int history_index = 0;
+int history_count = 0;
+
+
+// -----------
+// Keeping track of time
 
 uint8_t hours = 2;
 uint8_t minutes = 0;
 bool is_dst = false;
 
+String get_time() {
+  char formated_time[5];
+  sprintf(formated_time, "%02d:%02d", hours, minutes);
+  return (String)formated_time;
+}
+
+
+// -----------
+// HTTP server
+
+// Utils to return HTTP codes
+
+void replyOK() {
+  server.send(200, FPSTR(TEXT_PLAIN), "");
+}
+
+void replyOKWithMsg(String msg) {
+  server.send(200, FPSTR(TEXT_PLAIN), msg);
+}
+
+void replyNotFound(String msg) {
+  server.send(404, FPSTR(TEXT_PLAIN), msg);
+}
+
+void replyBadRequest(String msg) {
+  Serial.println(msg);
+  server.send(400, FPSTR(TEXT_PLAIN), msg + "\r\n");
+}
+
+void replyServerError(String msg) {
+  Serial.println(msg);
+  server.send(500, FPSTR(TEXT_PLAIN), msg + "\r\n");
+}
+
+/*
+   The "Not Found" handler catches all URI not explicitly declared in code
+   First try to find and return the requested file from the filesystem,
+   and if it fails, return a 404 page with debug information
+*/
+void handleNotFound() {
+  String uri = ESP8266WebServer::urlDecode(server.uri()); // required to read paths with blanks
+
+  // Dump debug data
+  String message;
+  message.reserve(100);
+  message = F("Error: File not found\n\nURI: ");
+  message += uri;
+  message += F("\nMethod: ");
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += F("\nArguments: ");
+  message += server.args();
+  message += '\n';
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += F(" NAME:");
+    message += server.argName(i);
+    message += F("\n VALUE:");
+    message += server.arg(i);
+    message += '\n';
+  }
+  message += "path=";
+  message += server.arg("path");
+  message += '\n';
+  Serial.print(message);
+
+  return replyNotFound(message);
+}
+
+String get_json_data() {
+  String json;
+  json.reserve(88);
+    
+  String moisture_data = "[";
+  String pump_data = "[";
+  String labels = "[";
+  bool first = true;
+  
+  for (int i = 0; i < history_count; i++) {
+    int x = i;
+    if (history_count == HISTORY_DEPTH) {
+      if (history_index + i >= HISTORY_DEPTH)
+        x = history_index + i - HISTORY_DEPTH;
+      else
+        x = history_index + i;
+    }
+
+    if (first == false) {
+      moisture_data += ", ";
+      pump_data += ", ";
+      labels += ", ";
+    }
+    first = false;
+    moisture_data += history_moisture[x]/1000.0;
+    pump_data += history_pump[x];
+    labels += i;
+  }
+  moisture_data += "]";
+  pump_data += "]";
+  labels += "]";
+
+  json = "{labels: " + labels + ", datasets: [{";
+  json += "  type: 'line',";
+  json += "  label: 'Soil moisture',";
+  json += "  data: " + moisture_data + ",";
+  json += "  fill: false,";
+  json += "  borderColor: 'rgb(54, 162, 235)',";
+  json += "  backgroundColor: 'rgb(54, 162, 235)',";
+  json += "  tension: 0.1";
+  json += "},{";
+  json += "  type: 'line',";
+  json += "  label: 'Pump',";
+  json += "  data: " + pump_data + ",";
+  json += "  fill: true,";
+  json += "  borderColor: 'rgb(255, 99, 132)',";
+  json += "  backgroundColor: 'rgba(255, 99, 132, 0.2)',";
+  json += "  tension: 0.1";
+  json += "}]};";
+
+  return json;
+}
+
+String get_webpage() {
+  String webpage = "<html><head><script src=\"https://cdn.jsdelivr.net/npm/chart.js\"></script></head>\n";
+  webpage += "<body><div><canvas id=\"myChart\"></canvas></div><script>\n";
+  webpage += "const data = " + get_json_data() + "\n";
+  webpage += "const config = {type: 'line', data: data};\n";
+  webpage += "const myChart = new Chart(document.getElementById('myChart'), config);\n";
+  webpage += "</script></body></html>";
+  return webpage;
+}
+
+void setup_http_server() {
+  
+  //get heap status, analog input value and all GPIO statuses in one json call
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/html", get_webpage());
+  });
+
+  // Default handler for all URIs not defined above
+  // Use it to read files from filesystem
+  server.onNotFound(handleNotFound);
+
+  // Start server
+  server.begin();
+}
+
+
+// -----------
+// 
 
 void connect_to_wifi() {
 
@@ -39,8 +210,7 @@ void connect_to_wifi() {
   Serial.print(SSID);
 
   // Initialisation
-  WiFi.persistent(false);
-  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);
 
   // Connecting to a WiFi network
   WiFi.begin(SSID, PASSWORD);
@@ -79,6 +249,9 @@ void get_internet_time() {
   hours = ptm->tm_hour;
   minutes = ptm->tm_min;
 
+  if (hours == 0 && minutes == 0)
+      ESP.restart();
+
   // Compute DST
   //  DST (GMT+1 durring summer time) starts on the last Sunday of March and ends on the last Sunday of October
   // True conditions :
@@ -95,12 +268,8 @@ void get_internet_time() {
   else
     is_dst = false;
 
-  Serial.println((String)"Internet time : " + hours + (String)":" + minutes);
+  Serial.println((String)"Internet time : " + get_time());
   Serial.println((String)"DST : " + is_dst);
-  Serial.println((String)"tm_mon : " + ptm->tm_mon);
-  Serial.println((String)"tm_mday : " + ptm->tm_mday);
-  Serial.println((String)"tm_wday : " + ptm->tm_wday);
-
 }
 
 void IRAM_ATTR TimerHandler() {
@@ -109,10 +278,12 @@ void IRAM_ATTR TimerHandler() {
 
 void stop_pump() {
   digitalWrite(PUMP, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void start_pump() {
   digitalWrite(PUMP, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void one_minute_int() {
@@ -126,7 +297,7 @@ void one_minute_int() {
     minutes++;
 
   if ((hours == WATERING_HOUR) && (minutes == WATERING_MINUTE)) {        
-    if (analogRead(MOISTURE_SENSOR) < MOISTURE_THREASHOLD){
+    if (analogRead(MOISTURE_SENSOR) > MOISTURE_THREASHOLD){
       start_pump(); 
       ISR_Timer.setTimeout(WATERING_DURATION_SEC * 1000, stop_pump);
     }
@@ -135,17 +306,19 @@ void one_minute_int() {
 
 void setup() {
   pinMode(PUMP, OUTPUT);
-  digitalWrite(PUMP, LOW);  
-
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MOISTURE_SENSOR, INPUT);
 
+  digitalWrite(PUMP, LOW);  
+  digitalWrite(LED_BUILTIN, HIGH);  
+  
   Serial.begin(115200);
   Serial.println();
 
-  Serial.println("Hello");
-
   connect_to_wifi();
   get_internet_time();
+
+  Serial.println((String)"Watering set for " + WATERING_HOUR + (String)":" + WATERING_MINUTE);
 
   // Interval in microsecs
   if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_MS * 1000, TimerHandler)) {
@@ -155,11 +328,30 @@ void setup() {
   }
   
   ISR_Timer.setInterval(TIMER_INTERVAL_60S, one_minute_int);
+
+  setup_http_server();
 }
 
+// Indication that one minute has passed
+esp8266::polledTimeout::periodicMs timeToGatherData(HISTORY_STEP_IN_SEC * 1000);
 
-void loop() {
+void loop() {  
+  server.handleClient();
+
+  if(timeToGatherData) {
     int moisture_sensor = analogRead(MOISTURE_SENSOR);
-    Serial.println(hours + (String)":" + minutes + (String)"Moisture sensor reading : " + moisture_sensor);
-    delay(60000);
+    
+    history_moisture[history_index] = moisture_sensor;
+    history_pump[history_index] = digitalRead(PUMP);
+  
+    Serial.println(get_time() + (String)" - Moisture sensor : " + moisture_sensor + " - Pump : " + digitalRead(PUMP));
+  
+    if (history_index-1 >= HISTORY_DEPTH)
+      history_index = 0;
+    else
+      history_index++;
+
+    if (history_count < HISTORY_DEPTH)
+      history_count++;
+  }  
 }
--- config.h
+++ config.h
@@ -4,11 +4,15 @@
 #define PASSWORD "<WIFI PASSWORD>"
 
 // IOs
-#define PUMP            D4
+#define PUMP            5 // D1
 #define MOISTURE_SENSOR A0
 
 // Watering parameters
 #define WATERING_HOUR         18
 #define WATERING_MINUTE       00
-#define WATERING_DURATION_SEC 60
+#define WATERING_DURATION_SEC 600
 #define MOISTURE_THREASHOLD   512
+
+// Data collection
+#define HISTORY_DEPTH       12*24*5
+#define HISTORY_STEP_IN_SEC 300
